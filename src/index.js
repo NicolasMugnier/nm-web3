8f7d74ffecac956ed8bdcd7c7dfab80d78b35ca9
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-02 10:40:25 +0000

    feat(farcaster): use frame sdk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useEffect, StrictMode } from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FrameSDK from '@farcaster/frame-sdk'
+
+function FarcasterFrameProvider({ children }) {
+  useEffect(() => {
+    const init = async () => {
+      setTimeout(() => {
+        FrameSDK.actions.ready()
+      }, 500)
+    }
+    init()
+  }, [])
+  return <>{children}</>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <FarcasterFrameProvider>
+      <App />
+    </FarcasterFrameProvider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
