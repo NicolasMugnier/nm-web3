0c459700025e8526b019fdee35fa034a16a4151f
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-13 19:32:14 +0000

    chore(refactor): clean config & remove webVitals

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import { useEffect, StrictMode } from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import FrameSDK from '@farcaster/frame-sdk'
+import FrameSDK from '@farcaster/frame-sdk';
 
 function FarcasterFrameProvider({ children }) {
   useEffect(() => {
@@ -25,8 +24,3 @@ root.render(
     </FarcasterFrameProvider>
   </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
